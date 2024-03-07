a2dfc01b445f20296350acd0623539f011205873
sudoarun <arunrajorya1999@gmail.com>
2024-03-07 17:06:55 +0530

    added home page and header for desktop

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,27 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Component</h1>
+      <section className="choosenSection">
+        <div className="container">
+          <div className="choosenContentHeader">
+            <span className="choosenHeader">
+              Want to be <span className="gradient-text">the chosen one</span>{" "}
+              for that dream job or promotion?
+            </span>
+            <p>
+              Apply to join the prestigious <br />
+              <bold>The Smart Fellowship: Soft Skills for Success program</bold>
+            </p>
+            <div>
+              <span className="d-block">Learn 8+ Futuristic Soft Skills</span>
+              <hr />
+              <span className="d-block">40 Hours ON Weekends only</span>
+              <hr />
+              <span className="d-block">GRADUATE WITH A Certificate</span>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
