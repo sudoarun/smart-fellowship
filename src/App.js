64eabc98d22f91fe947a6431bd94b8021d1f6817
karkill9 <kartikmm99@gmail.com>
2024-03-08 12:59:32 +0530

    Learning exp page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Home";
+import LearningExperience from "./pages/LearningExperience";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/learning" element={<LearningExperience />} />
       </Routes>
     </div>
   );
---
+++ src/pages/LearningExperience.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AdvantagesSection from "../components/Learning/AdvantagesSection";
+import Navbar from "../components/Header/Navbar";
+
+const LearningExperience = () => {
+  return (
+    <div>
+      <Navbar />
+      <AdvantagesSection />
+    </div>
+  );
+};
+
+export default LearningExperience;
