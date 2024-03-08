b43cfa5dda80833b406cccc2d768a56b70a302f4
Shivam <exceptionunique@gmail.com>
2024-03-08 13:35:46 +0530

    Menu Added

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./Menu.module.css";
+
+function Menu() {
+  return (
+    <div className={styles.menuSection}>
+
+        <div className={styles.icon}>
+           <img className={styles.cancelimg} src='/cancel.png' alt='cancel'></img>
+        </div>
+        <div className={styles.items}>
+             <br/> <h4 className={styles.item}>HOME</h4>
+             <br/><h4 className={styles.item}>LEARNING EXPERIENCE</h4>
+             <br/><h4 className={styles.item}>ABOUT</h4>
+             <br/><h4 className={styles.item}>CONTACT</h4>
+             <br/><h4 className={styles.item}>APPLY</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
